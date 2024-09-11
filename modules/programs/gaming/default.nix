bc330d5051fd32feb8cec4f59927aca03ec7491a
Ag1O1 <amrashraf20210@gmail.com>
2024-09-11 13:26:46 +0300

    gaming stuff

---
+++ modules/programs/gaming/default.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}:
+{
+  options.custom.gaming = {
+    enable = lib.mkEnableOption "gaming";
+  };
+  config = lib.mkIf config.custom.gaming.enable {
+    programs.steam.enable = true;
+    programs.steam.gamescopeSession.enable = true;
+    environment.systemPackages = with pkgs; [
+      mangohud
+      lutris
+    ];
+
+    programs.gamemode.enable = true;
+
+  };
+}
