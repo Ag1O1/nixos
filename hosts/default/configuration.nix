e720640c4a3dd71aa3e3b72977a4ba3bc9746d5b
Ag1O1 <amrashraf20210@gmail.com>
2024-09-15 00:32:38 +0300

    cleaning up

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -9,7 +9,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./fs.nix
+      ./hardware
       ./packages.nix
       (self + /modules)
     ];
@@ -18,7 +18,7 @@
   programs.kdeconnect.enable = true;
   programs.nix-ld.enable = false;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
+  nixpkgs.config.allowUnfree = true;
   networking.hostName = "nixos"; # Define your hostname.
 
   # Enable networking
@@ -37,49 +37,19 @@
     LC_TELEPHONE = "en_AG";
     LC_TIME = "en_AG";
   };
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  programs.direnv.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents..
-  services.printing.enable = true;
-
-  services.libinput.enable = true;
-  services.ollama.enable = true;
-  services.ollama.acceleration = "cuda";
-  services.open-webui.enable = true;
-  services.open-webui.openFirewall = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.amr = {
     isNormalUser = true;
     description = "Amr";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-
-    ];
   };
-home-manager = {
-  # also pass inputs to home-manager modules
-  extraSpecialArgs = {inherit inputs;};
-  users = {
-    "amr" = import ./home.nix;
+  home-manager = {
+    extraSpecialArgs = {inherit inputs;};
+    users = {
+      "amr" = import ./home.nix;
+    };
   };
-};
-  services.flatpak.enable = true;
+  
   modules.programs.gaming.enable = true;
   custom = {
     firefox.enable = true;
@@ -87,30 +57,39 @@ home-manager = {
     #gaming.enable = true;
     nvidia.enable = true;
   };
+
+  programs.direnv.enable = true;
+  services = {
+    xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+    xserver.enable = true;
+
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+
+    flatpak.enable = true;
+
+    printing.enable = true;
+    libinput.enable = true;
+    ollama.enable = true;
+    ollama.acceleration = "cuda";
+    open-webui.enable = true;
+    open-webui.openFirewall = true;
+    openssh.enable = true;
+  };
+
+
+
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
   system.stateVersion = "24.05"; # Don't change
-
 }
