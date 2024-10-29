19ec21c035274708ebbb7ad92bb6d7c4a3d0c0f4
Ag1O1 <amrashraf20210@gmail.com>
2024-10-29 11:14:03 +0300

    yeyey6e

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -1,16 +1,19 @@
 { pkgs, inputs, ... }:
 {
-  nix.settings = {
+  nix = {
     package = pkgs.lix;
-
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-    
     optimise.automatic = true;
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+    
+    settings = {
+      
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
   };
   programs.nix-ld.enable = false;
   nixpkgs.config.allowUnfree = true;
